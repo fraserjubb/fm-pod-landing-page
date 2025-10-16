5355cf8b63468693add722f11deed91630feb942
Fraser Jubb <fraser.jubb@gmail.com>
2025-10-16 18:44:42 +0100

    Fixed bug where success message would repeat. Also improved function names

--- script.js
+++ script.js
@@ -7,20 +7,21 @@ function createFeedback() {
   const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
   const feedbackElement = document.createElement('p');
+  feedbackElement.classList.add('hero__input-feedback');
 
   let feedbackContent;
   if (cleanedEmail.length === 0) {
     feedbackContent = document.createTextNode('Oops! Please add your email');
     emailInput.classList.add('hero__input--error-outline');
-    feedbackElement.classList.add('hero__input--error-message');
+    feedbackElement.classList.add('hero__input-feedback--error');
   } else if (!emailRegex.test(cleanedEmail)) {
     feedbackContent = document.createTextNode('Oops! Please check your email');
     emailInput.classList.add('hero__input--error-outline');
-    feedbackElement.classList.add('hero__input--error-message');
+    feedbackElement.classList.add('hero__input-feedback--error');
   } else {
     feedbackContent = document.createTextNode('Success!');
     emailInput.classList.add('hero__input--success-outline');
-    feedbackElement.classList.add('hero__input--success-message');
+    feedbackElement.classList.add('hero__input-feedback--success');
   }
 
   feedbackElement.appendChild(feedbackContent);
@@ -28,9 +29,9 @@ function createFeedback() {
 }
 
 function removeExistingFeedback() {
-  const existingError = heroForm.querySelector('.hero__input--error-message');
-  if (existingError) {
-    existingError.remove();
+  const existingFeedback = heroForm.querySelector('.hero__input-feedback');
+  if (existingFeedback) {
+    existingFeedback.remove();
     emailInput.classList.remove('hero__input--error-outline');
   }
 }
